e1077bdcb04d514c835cf40c45c2f46a7866fe27
Gunas12 <gunesmmmdova3@gmail.com>
2023-01-28 09:49:31 +0400

    last

--- client/src/Pages/Add/index.js
+++ client/src/Pages/Add/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
@@ -9,71 +9,75 @@ const SignupSchema = Yup.object().shape({
   img: Yup.string().required('Required'),
   name2: Yup.string().required('Required'),
   mark: Yup.string().required('Required'),
- 
+
 });
 function Index() {
-    const [data, setdata] = useState([])
-    return(
-        <Formik
-        initialValues={{
-         name: '',
-          name2: '',
-         about: '',
-          icon: '',
-          img: '',
-          mark: '',
-           }}
-        validationSchema={SignupSchema}
-        onSubmit={(values )=> {
-          const newdata={
-              name:values.name,
+  const [data, setdata] = useState([])
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        name2: '',
+        about: '',
+        icon: '',
+        img: '',
+        mark: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values, { resetForm }) => {
+        const newdata = {
+          name: values.name,
           name2: values.name2,
-         about: values.about,
-          icon:values.icon,
+          about: values.about,
+          icon: values.icon,
           img: values.img,
-          mark:values.mark
-          }
-         axios.post("http://localhost:7070/product",newdata)
-         setdata([...data, newdata ])
+          mark: values.mark
+        }
+        console.log(data);
+        axios.post("http://localhost:7070/product", newdata)
 
-         alert("adddd")
-          
-        }}
-      >
-        
-        {({ errors, touched }) => (
-          <Form className='container display-flex-center align-items-center mt-5'style={{marginTop:"200px"}}>
-            <Field  name="name" type="text" style={{marginTop:"200px"}}/><br/>
-            {errors.name && touched.name ? (
-              <div>{errors.name}</div>
-            ) : null}<br/>
-            <Field name="about" /><br/>
-            {errors.about && touched.about ? (
-              <div>{errors.about}</div>
-            ) : null}<br/>
-            <Field name="img" /><br/>
-            {errors.img && touched.img ? (
-              <div>{errors.img}</div>
-            ) : null}<br/>
-            <Field name="icon" /><br/>
-            {errors.icon && touched.icon ? (
-              <div>{errors.icon}</div>
-            ) : null}<br/>
-            <Field name="name2" /><br/>
-            {errors.name2 && touched.name2 ? (
-              <div>{errors.name2}</div>
-            ) : null}<br/>
-            <Field name="mark"/><br/>
-            {errors.mark && touched.mark ? (
-              <div >{errors.mark}</div>
-            ) : null}<br/>
-  
-            <button type="submit" className='btn btn-info'>Submit</button>
-          </Form>
-        )}
-       
-      </Formik>
- 
-)
-          }
+        setdata([...data, newdata])
+
+
+        alert("adddd")
+        resetForm({ values: '' })
+
+      }}
+    >
+
+      {({ errors, touched }) => (
+        <Form className='container display-flex-center align-items-center mt-5' style={{ marginTop: "200px" }}>
+          <Field name="name" type="text" style={{ marginTop: "200px" }} /><br />
+          {errors.name && touched.name ? (
+            <div>{errors.name}</div>
+          ) : null}<br />
+          <Field name="about" /><br />
+          {errors.about && touched.about ? (
+            <div>{errors.about}</div>
+          ) : null}<br />
+          <Field name="img" /><br />
+          {errors.img && touched.img ? (
+            <div>{errors.img}</div>
+          ) : null}<br />
+          <Field name="icon" /><br />
+          {errors.icon && touched.icon ? (
+            <div>{errors.icon}</div>
+          ) : null}<br />
+          <Field name="name2" /><br />
+          {errors.name2 && touched.name2 ? (
+            <div>{errors.name2}</div>
+          ) : null}<br />
+          <Field name="mark" type="number" /><br />
+          {errors.mark && touched.mark ? (
+            <div >{errors.mark}</div>
+          ) : null}<br />
+
+          <button type="submit" className='btn btn-info'>Submit</button>
+        </Form>
+      )}
+
+    </Formik>
+
+  )
+}
 export default Index
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -9,226 +9,226 @@ function Index() {
     const [loading, setloading] = useState(false)
     const [order, setorder] = useState("a")
     const [query, setquery] = useState("")
-    const sorting=(col)=>{
-        if(order==="a"){
-            const sorted=[...data].sort((a,b)=>b[col]-a[col])
+    const sorting = (col) => {
+        if (order === "a") {
+            const sorted = [...data].sort((a, b) => b[col] - a[col])
             setdata(sorted)
             setorder("b")
         }
-        if(order==="b"){
-            const sorted=[...data].sort((a,b)=>a[col]-b[col])
+        if (order === "b") {
+            const sorted = [...data].sort((a, b) => a[col] - b[col])
             setdata(sorted)
             setorder("a")
         }
 
     }
-    const getdata= async()=>{
+    const getdata = async () => {
         try {
             setloading(true)
-            let res=await axios.get("http://localhost:7070/product")
+            let res = await axios.get("http://localhost:7070/product")
             setloading(false)
             setdata(await res.data)
         } catch (error) {
             console.log(error);
-            
+
         }
     }
-    const handledel=(id)=>{
+    const handledel = (id) => {
         console.log(id);
-        axios.delete(`http://localhost:7070/product/${id}`).then(()=>getdata())
+        axios.delete(`http://localhost:7070/product/${id}`).then(() => getdata())
     }
     useEffect(() => {
-    getdata()
+        getdata()
     }, [])
-  return (
-   <>
-   <div className='section1'>
-   <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => swiper}
-    >
-      <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>  today!
-      </h1>
-      </SwiperSlide>
-      <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>  today!
-      </h1></SwiperSlide>
-      <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>today! 
-      </h1></SwiperSlide>
-     
-    </Swiper>
-    
-   </div>
-   <div className='section2'>
-    <div className='sechead'>
-      <div>  <hr className='hr22' />
-        <h3 className='h322'>Popular Courses</h3></div>
-        </div>
-<div className='container'>
-    <button className='btn btn-warning' onClick={()=>sorting("mark")}>Filter</button>
-    <input type="text" onClick={(e)=>{setquery(e.target.value)}}/>
-<div className='divcart'>
-    {
-        loading ? ("Loading..."):(
-            data.filter(item=> item.name.toLowerCase().includes(query.toLocaleLowerCase())).map((item, index)=>(
-                <div class="card"  key={index}>
-  <img src={item.img} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    
-    <h5 class="card-title">{item.name}</h5>
-    <p class="card-text">{item.about}</p>
-    <div className='row row2'>
-      <div className='col-1'>  <img className='icon' src={item.icon} alt="sekil"/></div>
-      <div className='col-8'> <span>{item.name2}</span></div>
-      <div className='col-3'><i>${item.mark}</i></div>
-      <button className="btn btn-danger" onClick={()=>handledel(item._id)}>Delete</button>
-    </div>
- 
-  </div>
-</div>
+    return (
+        <>
+            <div className='section1'>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => swiper}
+                >
+                    <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>  today!
+                    </h1>
+                    </SwiperSlide>
+                    <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>  today!
+                    </h1></SwiperSlide>
+                    <SwiperSlide className='imgdiv'><h1>Get your <span>Education</span>today!
+                    </h1></SwiperSlide>
 
-            ))
-            )
-    }
-</div>
-</div>
+                </Swiper>
 
-   </div>
-   <div className='section3'>
-    <div className='row'>
-        <div className='col-62 col-6'>
-            <div>
-            <h3>
-            Register now and get a discount <span className='text-dark '>50% </span>discount until 1 January
-            </h3>
-            <p>
-            In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempo.
-            </p>
-           
             </div>
-        </div>
-        <div className='col-6 col61'>
-            <div>
-            <h3>
-            Search for your course
-            </h3>
-           <input type="text" placeholder='COURSE NAME'/><br/>
-           <input type="text" placeholder='CATEGORY'/><br/>
-           <input type="text" placeholder='DEGREE'/><br/>
-           <input type="submit" value=" SEARCH COURSE" className='btn11'/><br/>
-            </div>
-        </div>
+            <div className='section2'>
+                <div className='sechead'>
+                    <div>  <hr className='hr22' />
+                        <h3 className='h322'>Popular Courses</h3></div>
+                </div>
+                <div className='container'>
+                    <button className='btn btn-warning' onClick={() => sorting("mark")}>Filter</button><br />
+                    Search <input type="text" onChange={(e) => { setquery(e.target.value) }} />
+                    <div className='divcart'>
+                        {
+                            loading ? ("Loading...") : (
+                                data.filter(item => item.name.toLowerCase().includes(query.toLocaleLowerCase())).map((item, index) => (
+                                    <div class="card" key={index}>
+                                        <img src={item.img} class="card-img-top" alt="..." />
+                                        <div class="card-body">
 
-    </div>
+                                            <h5 class="card-title">{item.name}</h5>
+                                            <p class="card-text">{item.about}</p>
+                                            <div className='row row2'>
+                                                <div className='col-1'>  <img className='icon' src={item.icon} alt="sekil" /></div>
+                                                <div className='col-8'> <span>{item.name2}</span></div>
+                                                <div className='col-3'><i>${item.mark}</i></div>
+                                                <button className="btn btn-danger" onClick={() => handledel(item._id)}>Delete</button>
+                                            </div>
 
-   </div>
-   <div className='section4'>
-   <div className='sechead'>
-      <div>  <hr className='hr22' />
-        <h3 className='h322'>Our Services</h3></div></div>
-    <div className='container'>
-        <div className='row'>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-        </div>
-        <div className='row'>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-            <div className='col-4'>
-                <img src='https://preview.colorlib.com/theme/course/images/exam.svg'/>
-                <h4>Online Courses</h4>
-                <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
-            </div>
-        </div>
+                                        </div>
+                                    </div>
 
-    </div>
-   
-   </div>
-   <div className='section5'>
-   <div className='sechead'>
-      <div>  <hr className='hr22' />
-        <h3 className='h322'>Our Services</h3></div></div>
-        <div className='container'>
-        <div className='row'>
-            
-            <div className='col-2'>
-               <h2>07</h2>
-               <p>January</p>
-            </div>
-            <div className='col-7'>
-                <h3>Student Festival</h3>
-                <h4>Grand Central Park</h4>
-                <p>
-                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
-                </p>
-            </div>
-            <div className='col-3'>
-                <img src="https://preview.colorlib.com/theme/course/images/event_1.jpg"/>
-            </div>
+                                ))
+                            )
+                        }
+                    </div>
+                </div>
 
-        </div>
-        <div className='row'>
-            
-            <div className='col-2'>
-               <h2>07</h2>
-               <p>January</p>
             </div>
-            <div className='col-7'>
-                <h3>Student Festival</h3>
-                <h4>Grand Central Park</h4>
-                <p>
-                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
-                </p>
-            </div>
-            <div className='col-3'>
-                <img src="https://preview.colorlib.com/theme/course/images/event_2.jpg"/>
-            </div>
+            <div className='section3'>
+                <div className='row'>
+                    <div className='col-62 col-6'>
+                        <div>
+                            <h3>
+                                Register now and get a discount <span className='text-dark '>50% </span>discount until 1 January
+                            </h3>
+                            <p>
+                                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempo.
+                            </p>
 
-        </div>
-        <div className='row'>
-            
-            <div className='col-2'>
-               <h2>07</h2>
-               <p>January</p>
-            </div>
-            <div className='col-7'>
-                <h3>Student Festival</h3>
-                <h4>Grand Central Park</h4>
-                <p>
-                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
-                </p>
-            </div>
-            <div className='col-3'>
-                <img src="https://preview.colorlib.com/theme/course/images/event_3.jpg"/>
-            </div>
+                        </div>
+                    </div>
+                    <div className='col-6 col61'>
+                        <div>
+                            <h3>
+                                Search for your course
+                            </h3>
+                            <input type="text" placeholder='COURSE NAME' /><br />
+                            <input type="text" placeholder='CATEGORY' /><br />
+                            <input type="text" placeholder='DEGREE' /><br />
+                            <input type="submit" value=" SEARCH COURSE" className='btn11' /><br />
+                        </div>
+                    </div>
 
-        </div>
-        </div>
-   </div>
-   </>
-  )
+                </div>
+
+            </div>
+            <div className='section4'>
+                <div className='sechead'>
+                    <div>  <hr className='hr22' />
+                        <h3 className='h322'>Our Services</h3></div></div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                        <div className='col-4'>
+                            <img src='https://preview.colorlib.com/theme/course/images/exam.svg' />
+                            <h4>Online Courses</h4>
+                            <p>In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div className='section5'>
+                <div className='sechead'>
+                    <div>  <hr className='hr22' />
+                        <h3 className='h322'>Our Services</h3></div></div>
+                <div className='container'>
+                    <div className='row'>
+
+                        <div className='col-2'>
+                            <h2>07</h2>
+                            <p>January</p>
+                        </div>
+                        <div className='col-7'>
+                            <h3>Student Festival</h3>
+                            <h4>Grand Central Park</h4>
+                            <p>
+                                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
+                            </p>
+                        </div>
+                        <div className='col-3'>
+                            <img src="https://preview.colorlib.com/theme/course/images/event_1.jpg" />
+                        </div>
+
+                    </div>
+                    <div className='row'>
+
+                        <div className='col-2'>
+                            <h2>07</h2>
+                            <p>January</p>
+                        </div>
+                        <div className='col-7'>
+                            <h3>Student Festival</h3>
+                            <h4>Grand Central Park</h4>
+                            <p>
+                                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
+                            </p>
+                        </div>
+                        <div className='col-3'>
+                            <img src="https://preview.colorlib.com/theme/course/images/event_2.jpg" />
+                        </div>
+
+                    </div>
+                    <div className='row'>
+
+                        <div className='col-2'>
+                            <h2>07</h2>
+                            <p>January</p>
+                        </div>
+                        <div className='col-7'>
+                            <h3>Student Festival</h3>
+                            <h4>Grand Central Park</h4>
+                            <p>
+                                In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor.
+                            </p>
+                        </div>
+                        <div className='col-3'>
+                            <img src="https://preview.colorlib.com/theme/course/images/event_3.jpg" />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Index
